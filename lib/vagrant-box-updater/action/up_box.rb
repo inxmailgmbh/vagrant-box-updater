c7858cce4d3a88d35d34f215782dfb7306f26421
joho <joho@inxmail.de>
2014-02-19 09:18:52 +0100

    Gives the user the power to decide when to apply an box update.
    Changes the update progress to ask the user to apply an available update until the update is applied (for every time the user calls 'vagrant up'). So far the vagrant-box-updater just asked once and if the user denies updating, the updater keeps track of this decision and ignores this update until another update is available. The auto-update behaviour is not affected by this commit.

--- lib/vagrant-box-updater/action/up_box.rb
+++ lib/vagrant-box-updater/action/up_box.rb
@@ -98,7 +98,7 @@ module VagrantPlugins
               env[:ui].warn("Modified image detected, this update set to be ignored until next change")
 	    else
               env[:ui].warn("Modified image detected : #{box_stats['url']} #{remote_modification_attribute}")
-	      if enable_autoupdate_flag == true || ask_confirm(env,"Would you like to update the box? \nIf negative - we keep ignoring this update, and notify only when another update detected. \nType (Y/N)")
+	      if enable_autoupdate_flag == true || ask_confirm(env,"Would you like to update the box? Type (Y/N) ")
                 env[:ui].info("Going to update and replace box \"#{box_name}\" now!")
                 provider = nil
                 env[:action_runner].run(Vagrant::Action.action_box_add, {
@@ -109,8 +109,8 @@ module VagrantPlugins
                   :box_download_insecure => true,
                  })
               else
-                env[:ui].warn("This update will be ignored")   
-	        Util::Common.add_box_stats(path_box_stat_file, {'ignored_image_attribute' => remote_modification_timestamp})
+                env[:ui].warn("Updating the box will not be performed.")   
+				# Util::Common.add_box_stats(path_box_stat_file, {'ignored_image_attribute' => remote_modification_timestamp})
                 @app.call(env)
               end
 	    end
